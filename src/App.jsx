fd90f63e3cebf04a87df3da28063fc1371c73c05
NipunaDakshina <87487225+NipunaDakshina@users.noreply.github.com>
2024-09-27 08:10:57 +0530

    setup the project with tailwind and react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//import { Navbar } from './components/Navbar'
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+     
+
+     <div className="p-8">
+        <h1 className="text-4xl text-center">Welcome to My Website</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
